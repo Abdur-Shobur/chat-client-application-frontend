9e178493734d476d7c3227d3c8055fb5745146c8
abdur-shobur <abdur.shobur.me@gmail.com>
2025-06-16 06:36:05 +0600

    Mobile screen warning add

--- frontend/app/(main)/layout.tsx
+++ frontend/app/(main)/layout.tsx
@@ -9,7 +9,15 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 	const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
 	return (
 		<>
-			<div className="flex-col flex">
+			<div className="flex-col flex lg:hidden h-screen items-center justify-center">
+				<div className="text-center">
+					<p className="text-lg">
+						Mobile view not supported yet please use a larger screen
+					</p>
+					<p className="text-sm text-red-400">We are working on it</p>
+				</div>
+			</div>
+			<div className="flex-col hidden lg:flex">
 				<Mail
 					defaultLayout={defaultLayout}
 					defaultCollapsed={defaultCollapsed}
