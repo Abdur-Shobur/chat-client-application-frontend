331735a0b0dcf231b5318fae7e160a2b600d5fc9
abdur-shobur <abdur.shobur.me@gmail.com>
2025-06-21 19:11:44 +0600

    update suspence

--- frontend/app/(main)/layout.tsx
+++ frontend/app/(main)/layout.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { AdminLayout } from '../components/admin-layout';
 import { UserLayout } from '../components/user-layout';
 import GroupJoin from '@/store/features/group/group.join';
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<>
-			{/* <div className="flex-col flex lg:hidden h-screen items-center justify-center">
-				<div className="text-center">
-					<p className="text-lg">
-						Mobile view not supported yet please use a larger screen
-					</p>
-					<p className="text-sm text-red-400">We are working on it</p>
-				</div>
-			</div> */}
+		<Suspense fallback={<div>Loading search params...</div>}>
 			<div className="flex-col lg:flex">
 				<AdminLayout>{children}</AdminLayout>
 				<UserLayout>{children}</UserLayout>
 			</div>
 
 			<GroupJoin />
-		</>
+		</Suspense>
 	);
 }
