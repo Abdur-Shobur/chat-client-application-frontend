63181feba1436a65eb1f7b942b2e0834a703565f
abdur-shobur <abdur.shobur.me@gmail.com>
2025-06-21 19:15:39 +0600

    update suspence

--- frontend/app/auth/page.tsx
+++ frontend/app/auth/page.tsx
@@ -1,10 +1,12 @@
 import { AuthTab } from '@/store/features/auth';
-import React from 'react';
+import React, { Suspense } from 'react';
 
 export default function page() {
 	return (
-		<div className="flex justify-center items-center h-screen">
-			<AuthTab />
-		</div>
+		<Suspense fallback={<div>Loading search params...</div>}>
+			<div className="flex justify-center items-center h-screen">
+				<AuthTab />
+			</div>
+		</Suspense>
 	);
 }
